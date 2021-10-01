7feb4f4f17f7290fef0de57db72e33f38c92d4e5
Jose Alvarez <alvarez.jo.developer@gmail.com>
2021-10-01 07:00:42 +0100

    Add unit tests for midpoint (hand-computed and exact)

--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,3 @@
-const secrets = require("../secrets.json");
 const app = require("../app");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
@@ -8,6 +7,9 @@ chai.should();
 chai.use(chaiHttp);
 
 const request = require("supertest");
+const secrets = require("../secrets.json");
+const projectId = secrets.GCLOUD_PROJECT_ID;
+const projectLocation = secrets.GCLOUD_PROJECT_LOCATION;
 
 describe("gae_node_request_example", () => {
   describe("GET /", () => {
@@ -25,8 +27,8 @@ describe("TEST Google Cloud Functions", () => {
   /**
    * Test trapezoidal function
    */
-  describe(`POST https://${secrets.GCLOUD_PROJECT_LOCATION}-${secrets.GCLOUD_PROJECT_ID}.cloudfunctions.net/trapezoidal`, () => {
-    it("Should return the result of the numerical method (trapezoidal rule)", (done) => {
+  describe(`POST https://${projectLocation}-${projectId}.cloudfunctions.net/trapezoidal`, () => {
+    it("Should return the hand-computed result (trapezoidal rule)", (done) => {
       const body = {
         f: "lambda t: 3*(t**2)*exp(t**3)",
         a: 0,
@@ -35,15 +37,60 @@ describe("TEST Google Cloud Functions", () => {
       };
 
       chai
-        .request(
-          `https://${secrets.GCLOUD_PROJECT_LOCATION}-${secrets.GCLOUD_PROJECT_ID}.cloudfunctions.net/`
-        )
+        .request(`https://${projectLocation}-${projectId}.cloudfunctions.net/`)
         .post("/trapezoidal")
         .send(body)
         .end((err, response) => {
           response.should.have.status(200);
-          // console.log(response);
-          response.should.property("text").eq("1.9227167504675762");
+          response.body.should.have.property("result");
+          response.body.should.have.property("result").eq(1.9227167504675762);
+          done();
+        });
+    });
+
+    it("Should return the hand-computed result (trapezoidal rule)", (done) => {
+      chai
+        .request(`https://${projectLocation}-${projectId}.cloudfunctions.net/`)
+        .post("/trapezoidal")
+        .send({
+          f: "lambda t: 3*(t**2)*exp(t**3)",
+          a: 0,
+          b: 1,
+          n: 2,
+        })
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.have.property("result");
+          response.body.should.have.property("result").eq(2.463642041244344);
+          done();
+        });
+    });
+
+    it("Should solve a problem without numerical errors (trapezoidal rule)", (done) => {
+      const F = (x) => {
+        return 3 * x ** 2 - 4 * x;
+      };
+      const a = 1.2;
+      const b = 4.4;
+      const expected = F(b) - F(a);
+      const tol = 1e-14;
+      const body = {
+        f: "lambda x: 6*x-4",
+        a: a,
+        b: b,
+        n: 2,
+      };
+      chai
+        .request(`https://${projectLocation}-${projectId}.cloudfunctions.net/`)
+        .post("/trapezoidal")
+        .send(body)
+        .end((err, response) => {
+          console.log(response.body);
+          response.should.have.status(200);
+          response.body.should.have.property("result");
+          response.body.should.have
+            .property("result")
+            .to.be.approximately(expected, tol);
           done();
         });
     });
